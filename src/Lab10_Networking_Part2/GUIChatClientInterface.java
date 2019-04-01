713422bf7c653c88bc0d666efd4de07fbc7c91f4
JiaGayoungSeo <gayoungs9397@gmail.com>
2019-04-01 14:32:51 -0600

    Lab10 GUI chat

--- src/Lab10_Networking_Part2/GUIChatClientInterface.java
+++ src/Lab10_Networking_Part2/GUIChatClientInterface.java
@@ -1,8 +1,16 @@
 package Lab10_Networking_Part2;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class GUIChatClientInterface extends JFrame {
+
+    private JTextArea chatInput;
+    private JTextArea chatOutput;
+    private JButton chatSend;
+
     public GUIChatClientInterface(){
         //set the title bar
         super("Chatter");
@@ -10,6 +18,59 @@ public class GUIChatClientInterface extends JFrame {
         setSize(500,500);
         //tell it to appear
         setVisible(true);
+
+        ////determines the layout
+        BorderLayout layout = new BorderLayout();
+        setLayout( layout );
+
+
+        //add some controls
+        chatOutput = new JTextArea();
+        chatInput = new JTextArea(3,20);
+        chatSend = new JButton("Out");
+
+
+        add(chatOutput, BorderLayout.CENTER);
+        add(chatInput, BorderLayout.SOUTH);
+        add(chatSend, BorderLayout.SOUTH);
+
+        Panel chatInputContainer = new Panel();
+
+        chatInputContainer.add(chatInput);
+        chatInputContainer.add(chatSend);
+
+        add( chatInputContainer,BorderLayout.SOUTH);
+
+        chatSend.addActionListener(
+
+                new ActionListener(){
+
+                    public void actionPerformed( ActionEvent ae){
+                        sendData(chatInput);
+                    }
+
+                    private void sendData(JTextArea out){
+
+
+                        try{
+
+                            System.out.println( out.getText() );
+
+                        }
+
+                        catch( Exception e){
+
+                            System.out.println("Oops! : "+e.toString() );
+
+                        }
+
+                    }
+
+
+                }
+        );
+
+
     }
 
     public static void main(String[] args){
